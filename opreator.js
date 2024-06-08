07f9723a0dd08e7f2109a59205efdf8dca6af381
Satyam Singh <147079047+satyam7991@users.noreply.github.com>
2024-06-08 18:03:14 +0530

    day1

--- opreator.js
+++ opreator.js
@@ -2,3 +2,27 @@ let name="satyam singh";
 console.log(name);
 
 console.log(typeof name);
+
+
+ let age = "33";
+ console.log(age);
+
+ console.log(typeof age);
+ console.log(typeof(age));
+
+ let change = Number (age);
+ console.log( typeof change);
+
+ console.log(1+"2");
+
+ console.log("1"+2);
+
+ console.log(1+2+"2");
+
+ console.log("1"+2+2);
+
+ const id = Symbol('123');
+ const userid = Symbol('123');
+ console.log(id === userid);
+
+
