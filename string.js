4ec5d3802e13ec7ca954ab0364d6d933a9e7af26
Satyam Singh <147079047+satyam7991@users.noreply.github.com>
2024-06-09 11:06:19 +0530

    day2

---
+++ string.js
@@ -0,0 +1,22 @@
+const str1 = "satyam singh";
+const str2 = " varanasi";
+console.log(str1+str2);
+
+console.log(`hello i am ${str1} and i am from ${str2}`);
+
+
+const gameName = new String('satyamsingh');
+console.log(gameName[2]);
+
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+
+console.log(gameName.toUpperCase());
+
+const name = '    shubham';
+console.log(name);
+console.log(name.trim());
+
+const url = "https://satyam.com/satyam20%singh";
+ console.log(url.replace('20%','-'));
