d487d14a68f7dc51f17929a9deded724d9d59cd7
Satyam Singh <147079047+satyam7991@users.noreply.github.com>
2024-10-03 14:27:22 +0530

    for each

--- foreach.js
+++ foreach.js
@@ -1,7 +1,7 @@
-// let arr=[12,23,34,45,56,6]
-// arr.forEach((value,index,array)=>{
-//     return console.log(value,index,array)
-// })
+let arr1=[12,23,34,45,56,6]
+arr.forEach((value,index,array)=>{
+    return console.log(value,index,array)
+})
 
 
 let arr=[12,3,4,5,6,7]
