48a6c0e9a776e0a61b248daaedf94da8bc5a67cf
Satyam Singh <147079047+satyam7991@users.noreply.github.com>
2024-10-07 11:28:41 +0530

    dom

---
+++ dom.js
@@ -0,0 +1,14 @@
+//1. get element by ID
+// 2. get element by Class
+// 3. get element by TagName
+// 4. get element by Query selector
+
+
+
+//  run time pr value change krne ke liye 
+// 1.innertext
+// 2.innerhtml
+
+
+// DOm:- dom is a document object model it is  a programing interface for web browsers
+// dom is a tree structure representing 
