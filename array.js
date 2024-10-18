5de068655012dea4f0a508367a5b8fc25783c139
Satyam Singh <147079047+satyam7991@users.noreply.github.com>
2024-10-18 21:04:30 +0530

    letrals

--- array.js
+++ array.js
@@ -3,13 +3,13 @@ let data=[10,20,30,40,50,60,70.2,"kiet",msg];
 console.log(data);
 data[8]();
 
-// for(let i=0;i<data.length;i++){
-//     console.log(`value of $(i) is ${data[i]}`); //string litterls
-// }
+for(let i=0;i<data.length;i++){
+    console.log(`value of $(i) is ${data[i]}`); //string litterls
+}
 
-// for(let index in data){
-//     console.log(`value of ${index} is ${data[index]}`);
-// }
+for(let index in data){
+    console.log(`value of ${index} is ${data[index]}`);
+}
 
 for(let value of data){
     console.log(`value of array=${value}`);
