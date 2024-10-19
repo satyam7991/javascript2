05f2b5884e6e2d3d02e7f3ee71ff3dbf6d44726e
Satyam Singh <147079047+satyam7991@users.noreply.github.com>
2024-10-19 21:37:22 +0530

    arrow function

--- arrowfun.js
+++ arrowfun.js
@@ -9,11 +9,11 @@ sum(23,25);
 const mul=(num1,num2)=>((num1+num2));
 console.log("multiply=",mul(23,2));
 
-// const mul=(num1,num2)=>{
-//     return num1*num2;
-// }
-// console.log("mull=" mul(23,3));
-const mult=(num1,num2)=>console.log("mul=",(num1*num2));
+const mult=(num1,num2)=>{
+    return num1*num2;
+}
+console.log("mull=" mul(23,3));
+const mul=(num1,num2)=>console.log("mul=",(num1*num2));
 
 
 
