1ae6e3356945735caa27a176ce8684d5fdfdbf85
Satyam Singh <147079047+satyam7991@users.noreply.github.com>
2024-10-26 14:11:31 +0530

    promises

--- es6/promise.js
+++ es6/promise.js
@@ -21,3 +21,16 @@ const resultInfo=async ()=>{
    
 }
 resultInfo();
+
+
+myPromise
+  .then((value) => `${value} and bar`)
+  .then((value) => `${value} and bar again`)
+  .then((value) => `${value} and again`)
+  .then((value) => `${value} and again`)
+  .then((value) => {
+    console.log(value);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
